eb9988b2ac38bbbdbe8a31040aa9d79ebf8a2a8a
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-02-28 05:55:41 +0000

    Корректировка кода

--- gradlew.bat
+++ gradlew.bat
@@ -8,3 +8,13 @@
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem
+
+@if "%DEBUG%"=="" @echo off
+@rem ##########################################################################
+@rem
