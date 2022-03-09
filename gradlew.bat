77bfb166721c1e51efcd722410adc7b8903e554a
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-03-09 19:16:46 +0000

    updated dependecies

--- gradlew.bat
+++ gradlew.bat
@@ -51,3 +51,9 @@ echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
 
 goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto execute
