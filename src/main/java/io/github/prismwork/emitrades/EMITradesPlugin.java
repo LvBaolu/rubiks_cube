d6267b5ca5c2b711c7dffc2eb8e3b0cec34c85b0
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-03-28 14:06:34 +0000

    Updated version string

--- src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
+++ src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
@@ -2,3 +2,9 @@
 package io.github.prismwork.emitrades;
 
 import com.google.common.collect.ImmutableSet;
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.stack.EmiStack;
+import io.github.prismwork.emitrades.config.EMITradesConfig;
+import io.github.prismwork.emitrades.recipe.VillagerTrade;
