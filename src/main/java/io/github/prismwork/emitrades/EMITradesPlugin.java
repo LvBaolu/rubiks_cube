441629e5e64f12eac7145ff9c86cbca4de3c9187
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-04-12 07:34:07 +0000

    :confetti_ball: Added .gitattributes

--- src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
+++ src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
@@ -104,3 +104,10 @@ public class EMITradesPlugin implements EmiPlugin {
         TradeOffers.WANDERING_TRADER_TRADES.forEach((lvl, offers) -> {
             for (TradeOffers.Factory offer : offers) {
                 if (isVanillaFactory(offer)) {
+                    registry.addRecipe(new VillagerTrade(new TradeProfile.DefaultImpl(WANDERING_TRADER_PLACEHOLDER, offer, lvl, wanderingTrader), wanderingTraderId.get()));
+                    wanderingTraderId.getAndIncrement();
+                } else {
+                    try {
+                        int attempts = 5;
+                        TreeSet<TradeOffer> genOffers = new TreeSet<>(this::compareOffers);
+                        TradeOffer inOffer;
