7d4c3627c02335758c4fbe492338655e99dc7bbc
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-05-05 13:17:03 +0000

    113.43+70

--- src/main/java/io/github/prismwork/emitrades/config/EMITradesConfig.java
+++ src/main/java/io/github/prismwork/emitrades/config/EMITradesConfig.java
@@ -7,3 +7,9 @@ import io.github.prismwork.prismconfig.api.config.DefaultSerializers;
 import io.github.prismwork.prismconfig.libs.jankson.Comment;
 
 import java.io.*;
+
+public class EMITradesConfig {
+    public static Config load(File file) {
+        if (!file.getName().endsWith(".json5"))
+            throw new RuntimeException("Failed to read config");
+        Config cfg = null;
