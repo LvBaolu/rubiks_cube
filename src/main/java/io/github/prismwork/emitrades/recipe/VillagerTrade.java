95b68d06602fbd57b18a44ffef616bb0df9b661f
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-05-28 22:51:37 +0000

    2004-02-28  Andrew Cagney  <cagney@redhat.com>

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -8,3 +8,7 @@ import dev.emi.emi.api.render.EmiTexture;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.stack.ListEmiIngredient;
+import dev.emi.emi.api.widget.SlotWidget;
+import dev.emi.emi.api.widget.WidgetHolder;
+import io.github.prismwork.emitrades.EMITradesPlugin;
+import io.github.prismwork.emitrades.util.EntityEmiStack;
