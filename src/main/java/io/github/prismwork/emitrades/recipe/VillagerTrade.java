d32dbffa0078f7708fa46aff01dc281378858a54
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-06-10 09:21:14 +0000

    Agenda

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -72,3 +72,10 @@ public class VillagerTrade implements EmiRecipe {
         } else if (offer instanceof TradeOffers.ProcessItemFactory factory) {
             inputs.add(0, EmiStack.of(Items.EMERALD, factory.price));
             inputs.add(1, EmiStack.of(factory.secondBuy, factory.secondCount));
+            outputs.add(0, EmiStack.of(factory.sell, factory.sellCount));
+        } else if (offer instanceof TradeOffers.SellEnchantedToolFactory factory) {
+            inputs.add(0, EmiStack.of(Items.EMERALD, Math.min(factory.basePrice + 5, 64)));
+            inputs.add(1, EmiStack.EMPTY);
+            outputs.add(0, EmiStack.of(factory.tool));
+        } else if (offer instanceof TradeOffers.TypeAwareBuyForOneEmeraldFactory factory) {
+            List<EmiStack> stacks = new ArrayList<>();
