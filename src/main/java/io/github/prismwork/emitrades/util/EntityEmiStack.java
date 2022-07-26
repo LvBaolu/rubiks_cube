19c92eba2d06518e94259dbf6cafb0dde8e964f7
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-07-26 06:38:30 +0000

    Name fixes

--- src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
+++ src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
@@ -1,2 +1,8 @@
 
 package io.github.prismwork.emitrades.util;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import dev.emi.emi.EmiPort;
+import dev.emi.emi.EmiUtil;
+import dev.emi.emi.api.stack.EmiStack;
+import dev.emi.emi.screen.tooltip.RemainderTooltipComponent;
